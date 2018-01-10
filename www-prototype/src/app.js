ac460d44ac3f45e7b4c1c3c11201a649dfe257a7
Blake La Pierre <blakelapierre@gmail.com>
2018-01-10 13:38:10 -0500

    tags, notes, highlights...maybe more

--- www-prototype/src/app.js
+++ www-prototype/src/app.js
@@ -31,7 +31,10 @@ const {
   ADD_UNIT,
   CHECK_SELECTED_TEXT,
   INSERT_SELECTED_TEXT,
+  HIGHLIGHT_TAG,
   REMOVE_SELECTION,
+  SET_TAG,
+  SET_NOTES,
   TAG_SELECTION,
   TOGGLE_LOG_DISPLAY
 } = loggedUnits(FIRE_EVENT => ({
@@ -52,13 +55,27 @@ const {
     state.selections.unshift(selection);
     return state;
   },
+  'HIGHLIGHT_TAG': (state, tag) => {
+    state.highlightedTag = tag;
+    console.log('highlighted', tag);
+    return state;
+  },
   'REMOVE_SELECTION': (state, selection) => {
     const index = state.selections.indexOf(selection);
     if (index >= 0) state.selections.splice(index, 1);
     return state;
   },
+  'SET_TAG': (state, tag, {target:{value}}) => {
+    tag.tag = value;
+    return state;
+  },
+  'SET_NOTES': (state, tag, {target:{value}}) => {
+    tag.notes = value;
+    return state;
+  },
   'TAG_SELECTION': (state, selection, tag) => {
-    state.tags.push({selection, tag});
+    const notes = '';
+    state.tags.push({selection, tag, notes});
     return state;
   },
   'TOGGLE_LOG_DISPLAY': (state) => {
@@ -81,22 +98,22 @@ const Unit = ({unit}) => (
 
 const UnitAnnotator = ({selection}, {tags, mutation}) => (
   <unit-annotator>
-    <remove-button onClick={mutation(REMOVE_SELECTION, selection)} title="Remove">Remove</remove-button>
+    <remove-button onClick={mutation(REMOVE_SELECTION, selection)} title="Remove">Remove Selection</remove-button>
     <selection>{selection}</selection>
     <inputs>
       <input type="text" placeholder="add a tag..." autoFocus />
       <button onClick={(m => event => {m(previousElementSiblingValue(event)); event.target.previousElementSibling.value = ''; event.target.previousElementSibling.focus();})(mutation(TAG_SELECTION, selection))}>Tag</button>
     </inputs>
     <unit-tags>
-      {(Object.values(groupBy(tags, 'selection')[selection] || []).map(({tag}) => <TagEditor tag={tag} />))}
+      {(Object.values(groupBy(tags, 'selection')[selection] || []).map(tag => <TagEditor tag={tag} />))}
     </unit-tags>
   </unit-annotator>
 );
 
-const TagEditor = ({tag}) => (
+const TagEditor = ({tag}, {mutation}) => (
   <tag-editor>
-    <input type="text" value={tag} />
-    <textarea placeholder="notes"></textarea>
+    <input type="text" value={tag.tag} onBlur={mutation(SET_TAG, tag)} />
+    <textarea placeholder="notes" onBlur={mutation(SET_NOTES, tag)}></textarea>
   </tag-editor>
 );
 
@@ -114,16 +131,23 @@ const Selections = ({selections}) => (
   </selections>
 );
 
-const Tags = ({}, {tags}) => (
+const Tags = ({tags}, {}) => (
   <tags>
-    <span>Tags</span>
-    {Object.values(mapValues(groupBy(tags, 'tag'), (selections, tag) => <tag><name>{tag}</name> <count>({selections.length})</count>  <selections>{selections.map(s => <selection>{s.selection}</selection>)}</selections></tag>))}
+    <span>Tags:</span>
+    {Object.values(mapValues(groupBy(tags, 'tag'), (selections, tag) => <Tag tag={tag} selections={selections} />))}
   </tags>
 );
 
-const TagDetail = () => (
-  <tag-detail>
+const Tag = ({tag, selections}, {highlightedTag, mutation}) => (
+  <tag className={{'highlighted': tag === highlightedTag}} onClick={mutation(HIGHLIGHT_TAG, tag)}>
+    <name>{tag}</name>
+    <count>({selections.length})</count>
+  </tag>
+);
 
+const TagDetail = ({tag}, {tags}) => (
+  <tag-detail>
+    {JSON.stringify(filter(tags, ({tag: tag2}) => tag === tag2))}
   </tag-detail>
 );
 
@@ -137,7 +161,7 @@ const LogDisplay = ({}, {log, logDisplay, mutation}) => (
   </log>
 );
 
-const WWWPrototype = ({selections}, {mutation}) => (
+const WWWPrototype = ({highlightedTag, selections, tags}, {mutation}) => (
   <www-prototype>
     {externalMutation = mutation}
     <left>
@@ -147,8 +171,9 @@ const WWWPrototype = ({selections}, {mutation}) => (
     <right>
       <side-by-side>
         {selections.length > 0 ? <Selections selections={selections} /> : undefined}
-        <Tags />
-        <TagDetail />
+        &nbsp;
+        {tags.length > 0 ? <Tags tags={tags} /> : undefined}
+        {highlightedTag ? <TagDetail tag={highlightedTag} /> : undefined}
       </side-by-side>
       <LogDisplay />
     </right>
